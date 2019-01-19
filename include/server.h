8bd078dd7944a9668434daf42ca647e88f9e1585
xiangyin701 <xiangyin701@cs130.org>
2019-01-19 17:48:39 +0000

    Refactored server_main.cc
    1.Refactored server_main into main.cc, server.cc and session.cc
    2.Created header files
    
    Change-Id: Ia288af6844345f5f4359e5d3117e8f4ea695ac8e

---
+++ include/server.h
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+
+class server {
+  public:
+    server(boost::asio::io_service& io_service, short port)
+      : io_service_(io_service),
+        acceptor_(io_service, tcp::endpoint(tcp::v4(), port));
+  private:
+    void start_accept();
+    void handle_accept(session* new_session,
+      const boost::system::error_code& error);
+    boost::asio::io_service& io_service_;
+    tcp::acceptor acceptor_;
+}
