7f1f1d73a2761357c7a968dfb4f9c8fab525cac6
xiangyin701 <xiangyin701@cs130.org>
2019-01-20 22:18:58 +0000

    Added test cases examples from Assignment 1
    Change-Id: I35c193fe16e20ab07219a759549c11b36bf0cb05

---
+++ tests/config_parser_test.cc
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "config_parser.h"
+
+TEST(NginxConfigParserTest, SimpleConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("example_config", &out_config);
+
+  EXPECT_TRUE(success);
+}
---
+++ tests/config_parser_test_copy.cc
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "config_parser.h"
+
+TEST(NginxConfigParserTest, SimpleConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("example_config", &out_config);
+
+  EXPECT_TRUE(success);
+
+  // Unit test 
+}
