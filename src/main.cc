0aef002f56a2641563d8a7ccdf1c71f36a123285
Nurym Kudaibergen <nurymka@cs130.org>
2019-01-28 19:35:32 +0000

    refactor: Give a correct cli usage statement
    Change-Id: I9d548ce04d2cb4d05305ac6738664542891348e4

--- src/main.cc
+++ src/main.cc
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
   {
     if (argc != 2)
     {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      std::cerr << "Usage: server <port>\n";
       return 1;
     }
 
