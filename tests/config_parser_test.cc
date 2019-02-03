23813eace5e5f07c75f85082ba2601725feff93e
nate <nate.sookwongse@cs130.org>
2019-02-03 20:30:17 +0000

    Moved sample test config files into separate directory to cleanup tests/
    Change-Id: I1707547245fd12d68e84916f6793438432b417d1

--- tests/config_parser_test.cc
+++ tests/config_parser_test.cc
@@ -30,134 +30,136 @@ namespace {
   };
 
   TEST_F(ConfigParserTest, EmptyContent) {
-    bool success = parser->Parse("example_config_EmptyContent", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_EmptyContent", out_config);
     EXPECT_FALSE(success);
   }
   
   TEST_F(ConfigParserTest, Allowed_NoSemicolonInBlock) {
-    bool success = parser->Parse("example_config_Allowed_NoSemicolonInBlock", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_Allowed_NoSemicolonInBlock", out_config);
     EXPECT_TRUE(success);
   }
 
   TEST_F(ConfigParserTest, Allowed_NoSemicolonInMain) {
-    bool success = parser->Parse("example_config_Allowed_NoSemicolonInMain", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_Allowed_NoSemicolonInMain", out_config);
     EXPECT_TRUE(success);
   }
 
   TEST_F(ConfigParserTest, Allowed_SingleQuoteSlashProblem) {
-    bool success = parser->Parse("example_config_Allowed_SingleQuoteSlashProblem", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_Allowed_SingleQuoteSlashProblem", out_config);
     EXPECT_TRUE(success);
   }
 
   TEST_F(ConfigParserTest, Allowed_DoubleQuoteSlashProblem) {
-    bool success = parser->Parse("example_config_Allowed_DoubleQuoteSlashProblem", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_Allowed_DoubleQuoteSlashProblem", out_config);
     EXPECT_TRUE(success);
   }
   
   TEST_F(ConfigParserTest, Example_Config) {
-    bool success = parser->Parse("example_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config", out_config);
     EXPECT_TRUE(success);
   }
 
   TEST_F(ConfigParserTest, NotAllowed_SemicolonAfterBraces) {
-    bool success = parser->Parse("example_config_NotAllowed_SemicolonAfterBraces", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_NotAllowed_SemicolonAfterBraces", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, NotAllowed_LackDoubleQuote1) {
-    bool success = parser->Parse("example_config_NotAllowed_LackDoubleQuote1", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_NotAllowed_LackDoubleQuote1", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, NotAllowed_LackDoubleQuote2) {
-    bool success = parser->Parse("example_config_NotAllowed_LackDoubleQuote2", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_NotAllowed_LackDoubleQuote2", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, NotAllowed_LackSingleQuote1) {
-    bool success = parser->Parse("example_config_NotAllowed_LackSingleQuote1", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_NotAllowed_LackSingleQuote1", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, NotAllowed_LackSingleQuote2) {
-    bool success = parser->Parse("example_config_NotAllowed_LackSingleQuote2", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_NotAllowed_LackSingleQuote2", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, NotAllowed_SingleQuoteSlashProblem) {
-    bool success = parser->Parse("example_config_NotAllowed_SingleQuoteSlashProblem", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_NotAllowed_SingleQuoteSlashProblem", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, NotAllowed_DoubleQuoteSlashProblem) {
-    bool success = parser->Parse("example_config_NotAllowed_DoubleQuoteSlashProblem", out_config);
+    bool success = parser->Parse("config_parser_test_input/example_config_NotAllowed_DoubleQuoteSlashProblem", out_config);
     EXPECT_FALSE(success);
   }
 
   // copying over tests from Nate's Assignment 1
   TEST_F(ConfigParserTest, SimpleContextConfig) {
-    bool success = parser->Parse("simple_context_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/simple_context_config", out_config);
     EXPECT_TRUE(success);
     EXPECT_EQ(1, out_config->statements_.size());
   }
 
 
   TEST_F(ConfigParserTest, MultipleContextConfig) {
-    bool success = parser->Parse("multiple_context_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/multiple_context_config", out_config);
     EXPECT_TRUE(success);
     EXPECT_EQ(2, out_config->statements_.size());
   }
 
   TEST_F(ConfigParserTest, ExtraEndBlockConfig) {
-    bool success = parser->Parse("extra_end_block_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/extra_end_block_config", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, EndBlockWithoutStartConfig) {
-    bool success = parser->Parse("end_block_without_start_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/end_block_without_start_config", out_config);
     EXPECT_FALSE(success);
   }
 
   TEST_F(ConfigParserTest, MissingEndBlockConfig) {
-    bool success = parser->Parse("missing_end_block_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/missing_end_block_config", out_config);
     EXPECT_FALSE(success);
   }
 
   // Example at http://nginx.org/en/docs/beginners_guide.html#conf_structure shows
   // an empty Location block
   TEST_F(ConfigParserTest, EmptyBlockConfig) {
-    bool success = parser->Parse("empty_block_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/empty_block_config", out_config);
     EXPECT_TRUE(success);
   }
 
   TEST_F(ConfigParserTest, ExtraSemicolonConfig) {
-    bool success = parser->Parse("extra_semicolon_config", out_config);
+    bool success = parser->Parse("config_parser_test_input/extra_semicolon_config", out_config);
     EXPECT_FALSE(success);
   }
 
+  // Test Port config parsing
+  
   TEST_F(ConfigParserTest, PortConfigInHttpBlock) {
-    bool success = parser->Parse("port_config_http_block", out_config);
+    bool success = parser->Parse("config_parser_test_input/port_config_http_block", out_config);
     EXPECT_TRUE(success);
     int portNo = out_config->getPort();
     EXPECT_EQ(portNo, 8080);
   }
 
   TEST_F(ConfigParserTest, PortConfigInServerBlock) {
-    bool success = parser->Parse("port_config_server_block", out_config);
+    bool success = parser->Parse("config_parser_test_input/port_config_server_block", out_config);
     EXPECT_TRUE(success);
     int portNo = out_config->getPort();
     EXPECT_EQ(portNo, 8080);
   }
 
   TEST_F(ConfigParserTest, PortConfigInFirstStatement) {
-    bool success = parser->Parse("port_config_listen_statement", out_config);
+    bool success = parser->Parse("config_parser_test_input/port_config_listen_statement", out_config);
     EXPECT_TRUE(success);
     int portNo = out_config->getPort();
     EXPECT_EQ(portNo, 8080);
   }
 
   TEST_F(ConfigParserTest, PortConfigNoFile) {
-    bool success = parser->Parse("FILE_DOES_NOT_EXIST", out_config);
+    bool success = parser->Parse("config_parser_test_input/FILE_DOES_NOT_EXIST", out_config);
     EXPECT_FALSE(success);
     int portNo = out_config->getPort();
     EXPECT_EQ(portNo, -1);
