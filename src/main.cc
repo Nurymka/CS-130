3634c04461e3de129b7b251c3324967e517497d4
Nurym Kudaibergen <nurymka@cs130.org>
2019-02-06 06:21:28 +0000

    feat(main.cc): Removed redundant severity levels, added extra logs
    Change-Id: Iedb79d447e58a9b06393ce5a36a96fa52b1f8550

--- src/main.cc
+++ src/main.cc
@@ -24,31 +24,31 @@ using namespace std;
 int main(int argc, char* argv[]) {
   try {
     if (argc != 2) {
-      BOOST_LOG_SEV(Logger::get(), ERROR) << "<error>: Wrong number of arguments for server initialization.\n";
-      // std::cerr << "Usage: server <port>\n";
+      BOOST_LOG_SEV(Logger::get(), CRITICAL)
+        << "Wrong number of arguments for server initialization.\n"
+        << "Usage: server <port>\n";
       return 1;
     }
 
     // copied code from Assignment 1 to find out the portNumber from target config file
     NginxConfigParser config_parser;
     NginxConfig config;
-    if (config_parser.Parse(argv[1], &config) == false) {
-      BOOST_LOG_SEV(Logger::get(), ERROR) << "<error>: Error encountered parsing config file.\n";
+    if (!config_parser.Parse(argv[1], &config)) {
+      BOOST_LOG_SEV(Logger::get(), ERROR)
+        << "Error encountered parsing config file.";
     }
     int16_t portNumber = (int16_t)config.getPort();
-    BOOST_LOG_SEV(Logger::get(), INFO) << "<info>: Port number is: " << portNumber;
 
     unordered_map<string, HandlerMaker*> targetToHandler = config.getTargetToHandler();
     HandlerManager* handlerManager = new HandlerManager(targetToHandler);
     boost::asio::io_service io_service;
-    // std::cout<<"starting service at port"<< std::endl;
-    // std::cout<<portNumber<< std::endl;
-    server s(io_service, portNumber, handlerManager);
 
+    server s(io_service, portNumber, handlerManager);
     io_service.run();
   }
   catch (std::exception& e) {
-    std::cerr << "Exception: " << e.what() << "\n";
+    BOOST_LOG_SEV(Logger::get(), CRITICAL)
+      << "Exception: " << e.what();
   }
 
   return 0;
