f1ae58917404008774448e15b18ee94702dcb6d6
Nurym Kudaibergen <nurymka@cs130.org>
2019-02-25 20:28:01 +0000

    feat(multithreading_test): Added an integration test for concurrent calls to /status
    Change-Id: I488b09af6a6eb39cbb5cfbaa2ea81aa020619368

--- tests/multithreading_test.sh
+++ tests/multithreading_test.sh
@@ -3,6 +3,7 @@
 PORT_NO=8080
 SERVER_URL="http://localhost:$PORT_NO"
 TIMEOUT=10
+NUM_STATUS_CALLS=100
 
 # generate tmp config
 rm -rf .tmp/
@@ -16,6 +17,10 @@ root $ROOT;
 
 handler echo {
   location /echo;
+}
+
+handler status {
+  location /status;
 }
 
 handler sleep {
@@ -32,9 +37,9 @@ kill_server() {
   kill -9 "$SERVER_PID"
 }
 
-# Run the tests
 OUTPUT_FILE=".tmp/multithreading_test.out"
 
+# Test that a request to /sleep doesn't block other requests
 echo "Testing multithreading support..."
 curl "$SERVER_URL/sleep" &> /dev/null &
 
@@ -57,5 +62,30 @@ else
   exit 1
 fi
 
+# Test that on 10 calls to /status, threads don't step on each other
+echo "Testing race conditions on /status calls..."
+for i in `seq 1 $NUM_STATUS_CALLS`;
+do
+  curl "$SERVER_URL/status" &> /dev/null &
+  pid=$!
+  PID_LIST+=" $pid"
+done
+
+trap "kill $PID_LIST" SIGINT
+
+wait $PID_LIST
+curl "$SERVER_URL/status" -sv &> "$OUTPUT_FILE"
+
+# -A1 returns the next line after match, so we can grep again for count of reqs
+cat "$OUTPUT_FILE" | grep -A1 "/status" | grep "200: $NUM_STATUS_CALLS"
+if [ $? == 0 ];
+then
+  echo "SUCCESS... /status calls returned $NUM_STATUS_CALLS request counts"
+else
+  echo "FAILURE... /status calls didn't return $NUM_STATUS_CALLS request counts"
+  kill_server
+  exit 1
+fi
+
 kill_server
-exit 1
+exit 0
