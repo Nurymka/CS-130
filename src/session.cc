b19c426b227af86fe5402cf7fe12fd2b696f2341
Nurym Kudaibergen <nurymka@cs130.org>
2019-02-25 20:27:36 +0000

    feat(session): Wrapped all session functions in try/catch
    In case threads fail in session.cc, the server would crash without try/catch blocks.
    
    Change-Id: I2b1bfdf41cf134406492ba2629ade799b043f015

--- src/session.cc
+++ src/session.cc
@@ -53,104 +53,123 @@ tcp::socket& session::socket() {
 }
 
 void session::start() {
-  socket_.async_read_some(boost::asio::buffer(_buffer, buffer_length),
-      boost::bind(&session::handle_read, this,
-        boost::asio::placeholders::error,
-        boost::asio::placeholders::bytes_transferred));
-  BOOST_LOG_SEV(Logger::get(), INFO)
-    << "Established connection with: "
-    << socket().remote_endpoint().address().to_string();
+  try {
+    socket_.async_read_some(boost::asio::buffer(_buffer, buffer_length),
+        boost::bind(&session::handle_read, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+    BOOST_LOG_SEV(Logger::get(), INFO)
+      << "Established connection with: "
+      << socket().remote_endpoint().address().to_string();
+  }
+  catch (std::exception& e) {
+    BOOST_LOG_SEV(Logger::get(), CRITICAL)
+      << "Exception: " << e.what();
+  }
 }
 
 void session::handle_read(const boost::system::error_code& error,
       size_t bytes_transferred) {
-  string client_ip = socket().remote_endpoint().address().to_string();
+  try {
+    string client_ip = socket().remote_endpoint().address().to_string();
 
-  if (!error) {
-    BOOST_LOG_SEV(Logger::get(), INFO)
-      << "Starting to read bytes from "
-      << client_ip;
-    int buffer_len = sizeof(_buffer);
-    input_.assign(_buffer, _buffer + bytes_transferred);
-    if (bytes_transferred < buffer_length) {
-      string str(input_.begin(), input_.end());
+    if (!error) {
       BOOST_LOG_SEV(Logger::get(), INFO)
-        << "Read " << buffer_len - 1 << " bytes from " << client_ip;
-      BOOST_LOG_SEV(Logger::get(), INFO)
-        << "Raw request from " << client_ip << ":\n" << str;
-      HttpRequest req;
-      unique_ptr<HttpResponse> res;
-      bool success = req.parse(str);
-      unique_ptr<Handler> handler;
-
-      if (success) {
+        << "Starting to read bytes from "
+        << client_ip;
+      int buffer_len = sizeof(_buffer);
+      input_.assign(_buffer, _buffer + bytes_transferred);
+      if (bytes_transferred < buffer_length) {
+        string str(input_.begin(), input_.end());
         BOOST_LOG_SEV(Logger::get(), INFO)
-          << "Handling valid request from " << client_ip;
-        LocationInfo* locInfo = LocationUtils::getLongestMatchingLocation(req.target, *locationMap_);
+          << "Read " << buffer_len - 1 << " bytes from " << client_ip;
+        BOOST_LOG_SEV(Logger::get(), INFO)
+          << "Raw request from " << client_ip << ":\n" << str;
+        HttpRequest req;
+        unique_ptr<HttpResponse> res;
+        bool success = req.parse(str);
+        unique_ptr<Handler> handler;
 
-        if (locInfo) {
-          string regLocation;
-          locInfo->blockConfig->getTopLevelStatement("location", regLocation);
+        if (success) {
           BOOST_LOG_SEV(Logger::get(), INFO)
-            << "Requested location " << req.target
-            << " is served by handler registered at " << regLocation;
-          BOOST_LOG_SEV(Logger::get(), INFO)
-            << "Handler type is '" << locInfo->handlerType << "'";
-          handler = HandlerManager::createByName(locInfo->handlerType,
-                                                *locInfo->blockConfig,
-                                                rootPath_);
+            << "Handling valid request from " << client_ip;
+          LocationInfo* locInfo =
+            LocationUtils::getLongestMatchingLocation(req.target, *locationMap_);
+
+          if (locInfo) {
+            string regLocation;
+            locInfo->blockConfig->getTopLevelStatement("location", regLocation);
+            BOOST_LOG_SEV(Logger::get(), INFO)
+              << "Requested location " << req.target
+              << " is served by handler registered at " << regLocation;
+            BOOST_LOG_SEV(Logger::get(), INFO)
+              << "Handler type is '" << locInfo->handlerType << "'";
+            handler = HandlerManager::createByName(locInfo->handlerType,
+                                                  *locInfo->blockConfig,
+                                                  rootPath_);
+          } else {
+            handler = HandlerManager::createByName("404", NginxConfig(), rootPath_);
+          }
         } else {
-          handler = HandlerManager::createByName("404", NginxConfig(), rootPath_);
+          BOOST_LOG_SEV(Logger::get(), INFO)
+            << "Handling bad request from " << client_ip;
+          handler = HandlerManager::createByName("bad_request", NginxConfig(), rootPath_);
         }
-      } else {
+        res = handler->handle_request(req);
+
+        string responseStr = res->to_string();
+        const char* chars = responseStr.c_str();
         BOOST_LOG_SEV(Logger::get(), INFO)
-          << "Handling bad request from " << client_ip;
-        handler = HandlerManager::createByName("bad_request", NginxConfig(), rootPath_);
+          << "Sending a response to " << client_ip
+          << " of size " << responseStr.length() << " bytes";
+        boost::asio::async_write(socket_,
+          boost::asio::buffer(chars, responseStr.length()),
+          boost::bind(&session::handle_write, this,
+            boost::asio::placeholders::error));
+
+        dataStore_->recordRequest(req.target, res->status_code);
+      } else {
+        socket_.async_read_some(boost::asio::buffer(_buffer, buffer_length),
+          boost::bind(&session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
       }
-      res = handler->handle_request(req);
-
-      string responseStr = res->to_string();
-      const char* chars = responseStr.c_str();
-      BOOST_LOG_SEV(Logger::get(), INFO)
-        << "Sending a response to " << client_ip
-        << " of size " << responseStr.length() << " bytes";
-      boost::asio::async_write(socket_,
-        boost::asio::buffer(chars, responseStr.length()),
-        boost::bind(&session::handle_write, this,
-          boost::asio::placeholders::error));
-
-      dataStore_->recordRequest(req.target, res->status_code);
     } else {
-      socket_.async_read_some(boost::asio::buffer(_buffer, buffer_length),
-        boost::bind(&session::handle_read, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
+      BOOST_LOG_SEV(Logger::get(), ERROR)
+        << "Failed to read bytes from " << client_ip
+        << ": " << error.message();
+      delete this;
     }
-  } else {
-    BOOST_LOG_SEV(Logger::get(), ERROR)
-      << "Failed to read bytes from " << client_ip
-      << ": " << error.message();
-    delete this;
+  }
+  catch (std::exception& e) {
+    BOOST_LOG_SEV(Logger::get(), CRITICAL)
+      << "Exception: " << e.what();
   }
 }
 
 void session::handle_write(const boost::system::error_code& error) {
-  string client_ip = socket().remote_endpoint().address().to_string();
+  try {
+    string client_ip = socket().remote_endpoint().address().to_string();
 
-  if (!error) {
-    // TODO(nurymka): It is actually not the case that a connection always shuts
-    // down here.
-    BOOST_LOG_SEV(Logger::get(), INFO)
-      << "Successfully wrote to " << client_ip
-      << ". Shutting down connection.";
-    boost::system::error_code ignored_ec;
-    socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both,
-            ignored_ec);
-  } else {
-    BOOST_LOG_SEV(Logger::get(), ERROR)
-      << "Failed to write bytes to " << client_ip
-      << ": " << error.message();
-    delete this;
+    if (!error) {
+      // TODO(nurymka): It is actually not the case that a connection always shuts
+      // down here.
+      BOOST_LOG_SEV(Logger::get(), INFO)
+        << "Successfully wrote to " << client_ip
+        << ". Shutting down connection.";
+      boost::system::error_code ignored_ec;
+      socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both,
+                       ignored_ec);
+    } else {
+      BOOST_LOG_SEV(Logger::get(), ERROR)
+        << "Failed to write bytes to " << client_ip
+        << ": " << error.message();
+      delete this;
+    }
+  }
+  catch (std::exception& e) {
+    BOOST_LOG_SEV(Logger::get(), CRITICAL)
+      << "Exception: " << e.what();
   }
 }
 
