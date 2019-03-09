50b600c350b2b5413840aaa42d9ec1a85d07538f
nate <nate.sookwongse@cs130.org>
2019-03-09 00:34:40 +0000

    Escape meme attributes to be parsed by JSON properly
    Change-Id: I3d326ef4d084a5096daa5d239adef5c6e9ea94a6

--- src/data/meme.cc
+++ src/data/meme.cc
@@ -25,12 +25,33 @@ string Meme::getID() {
 	return this->id_;
 }
 
+// https://stackoverflow.com/questions/7724448/simple-json-string-escape-for-c
+std::string escape_json(const std::string& input) {
+    std::ostringstream ss;
+    for (auto iter = input.cbegin(); iter != input.cend(); iter++) {
+    //C++98/03:
+    //for (std::string::const_iterator iter = input.begin(); iter != input.end(); iter++) {
+        switch (*iter) {
+            case '\\': ss << "\\\\"; break;
+            case '"': ss << "\\\""; break;
+            case '/': ss << "\\/"; break;
+            case '\b': ss << "\\b"; break;
+            case '\f': ss << "\\f"; break;
+            case '\n': ss << "\\n"; break;
+            case '\r': ss << "\\r"; break;
+            case '\t': ss << "\\t"; break;
+            default: ss << *iter; break;
+        }
+    }
+    return ss.str();
+}
+
 string Meme::toJSON() {
 	stringstream meme_stream;
 	meme_stream << "{ ";
-	meme_stream << "\"img_path\" : \"" << this->img_path_ << "\", ";
-	meme_stream << "\"top_text\" : \"" << this->top_text_ << "\", ";
-	meme_stream << "\"bottom_text\" : \"" << this->bottom_text_ << "\", ";
+	meme_stream << "\"img_path\" : \"" << escape_json(this->img_path_) << "\", ";
+	meme_stream << "\"top_text\" : \"" << escape_json(this->top_text_) << "\", ";
+	meme_stream << "\"bottom_text\" : \"" << escape_json(this->bottom_text_) << "\", ";
 	meme_stream << "\"id\" : \"" << this->id_ << "\"";
 	meme_stream << " }";
 	return meme_stream.str();
